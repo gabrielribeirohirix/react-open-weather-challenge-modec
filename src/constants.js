da31124ec6426a4fca14254c120b3544bc345d2d
Gabriel Ribeiro <gabriel.ribeiro@ipremios.com>
2020-10-04 23:09:54 -0300

    Adding changes on url

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const constants = {
 
     googleMapsAPIKey: "AIzaSyAUkcQtF2LtiuBEBaTCohAmgPvFPYjH-UM",
     openWeatherAPIKey: "b3aaffea2627fb905a2a099897db3009",
-    openWeatherAPIUrl: "http://api.openweathermap.org/data/2.5/find?",
+    openWeatherAPIUrl: "https://api.openweathermap.org/data/2.5/find?",
     temperatureConversionArray: [
         { value: "celsius", label: "Celsius" },
         { value: "kelvin", label: "Kelvin" },
